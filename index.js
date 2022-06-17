d544388fe89558f922454dffcb00bd96cbd6f5d5
Brain <ortiz_brian323@yahoo.com>
2022-06-17 16:00:44 -0700

    added viewAllEmployees function

--- index.js
+++ index.js
@@ -101,6 +101,32 @@ viewAllRoles = () => {
   });
 };
 
+viewAllEmployees = () => {
+  const sql = `SELECT 
+                employees.id, 
+                employees.first_name, 
+                employees.last_name,
+                roles.title, 
+                department.name AS departments, 
+                roles.salary, 
+                CONCAT (manager.first_name, ' ' , manager.last_name) AS manager
+              FROM employees
+                LEFT JOIN roles
+                ON employees.role_id = roles.id
+                LEFT JOIN department
+                ON roles.department_id = department.id
+                LEFT JOIN employees manager ON employees.manager_id = manager.id`;
+
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    userPrompt();
+  });
+
+
+};
+
+
 
 /*
 GIVEN a command-line application that accepts user input
